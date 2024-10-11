eb0abe2f8fbb6c77bd25ab2c9b7bdd9146f17381
PolBaldrich <polbaldrichsala@gmail.com>
2024-10-11 16:43:50 +0200

    primer

---
+++ elMeuCodi.js
@@ -0,0 +1,14 @@
+// Este archivo contiene el código JavaScript
+window.onload = function() {
+    alert('Hola Món');
+};
+
+var carrer = 'C/ de les "Sitges", 1';
+let elMeuNum = 5;
+let unAltreNumero = "5";
+console.log(elMeuNum);
+// elMeuNum++ ; // elMeuNum val 6 
+console.log(++elMeuNum);
+elMeuNum-- ; // elMeuNum val 5
+console.log(elMeuNum);
+prompt();
---
+++ exercicis/operadors.js
@@ -0,0 +1,5 @@
+///4.1.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+let a = 25;
+let b = 15 + a;
+let c = b * 2;
+console.log(a,b,c);
